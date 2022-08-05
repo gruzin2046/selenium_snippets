471e2ffcf1727e36c62c15839170a287872c4d44
aluniewi <albert.luniewicz@capgemini.com>
2022-08-05 11:05:04 +0200

    submit_correctCredentials_opensSecurePage() - fixe

--- src/main/java/p02_pom/heroku/HerokuSecurePage.java
+++ src/main/java/p02_pom/heroku/HerokuSecurePage.java
@@ -7,12 +7,12 @@ import org.openqa.selenium.WebElement;
 import static p02_pom.heroku.HerokuLoginPage.SELECTOR_FLASH;
 
 public class HerokuSecurePage {
-    private static final String LOGIN_PAGE_URL = "http://the-internet.herokuapp.com/secure";
+    //private static final String LOGIN_PAGE_URL = "http://the-internet.herokuapp.com/secure";
     private WebDriver driver;
 
     public HerokuSecurePage(WebDriver driver){
         this.driver = driver;
-        driver.get(LOGIN_PAGE_URL);
+        //driver.get(LOGIN_PAGE_URL);
     }
 
     public boolean isSuccessfullLoginMsgDisplayed() {
