180dddbf60cc24d4690b3370360e752feae4dc2d
Jakub Wrąbel <wrabel.jakub@gmail.com>
2022-08-16 16:59:06 +0200

    Revert "first 8 @Tests"
    This reverts commit 64a94281c9c08e50e8f40345b7c61ba77f41a999.

--- src/main/java/p02_pom/CustomersPage.java
+++ src/main/java/p02_pom/CustomersPage.java
@@ -3,7 +3,6 @@ package p02_pom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,17 +12,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 // Page Object
 public class CustomersPage {
     private static final String PAGE_URL = "http://68.183.78.90:81/";
+
     private static final By SELECTOR_CUSTOMER_BLOCK = By.className("Customer-data");
     private static final By SELECTOR_CUSTOMER_NAME = By.cssSelector("span.customer-name");
     // div.Customers-list strong.Delete-button
     private static final By SELECTOR_DELETE_BUTTON = By.cssSelector("strong.Delete-button");
-    private static final By SELECTOR_INPUT_FIELD = By.cssSelector("#add-customer-input");
-    private static final By SELECTOR_ADD_USER_BUTTON = By.cssSelector("#add-customer-button");
 
     private final WebDriver driver;
 
     public CustomersPage() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.home") + "/tools/webdrivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.home") + "/tools/webdrivers/chromedriver");
         System.setProperty("webdriver.chrome.whitelistedIps", "");
         driver = new ChromeDriver();
         driver.get(PAGE_URL);
@@ -56,26 +54,5 @@ public class CustomersPage {
 
     public void deleteFirstCustomer() {
         driver.findElement(SELECTOR_DELETE_BUTTON).click();
-    }
-
-    public void findInputField() {
-        driver.findElement(SELECTOR_INPUT_FIELD);
-    }
-
-    public void closeDriver() {
-        driver.quit();
-    }
-
-    public void addUser(String name) {
-        driver.findElement(SELECTOR_INPUT_FIELD).sendKeys(name);
-        driver.findElement(SELECTOR_ADD_USER_BUTTON).click();
-    }
-
-    public void deleteUser(String name) {
-        List<WebElement> userList = driver.findElements(SELECTOR_CUSTOMER_BLOCK).stream()
-                .filter(e -> e.findElement(SELECTOR_CUSTOMER_NAME).getText().equals(name))
-                .collect(Collectors.toList());
-        System.out.println(userList);
-        userList.stream().forEach(e -> e.findElement(SELECTOR_DELETE_BUTTON).click());
     }
 }
